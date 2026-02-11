df20e9ef9b7c31cb306f3db23623f18bd1a03103
badlandschugs <robloxfan2017@icloud.com>
2026-02-10 22:07:41 -0500

    STARTING TO MAKE THE ACTAUL WEBSITE NOW WOOOOO

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import "./App.css";
+
 function App() {
   return (
     <div>
-      <h1>Bointerbrate</h1>
+      <h1>Bointerbrate V3</h1>
+
+      <nav>
+        <div className="navLinks">
+          <a>Home</a>
+          <a>List</a>
+          <a>Rules</a>
+          <a>Submissions</a>
+        </div>
+      </nav>
     </div>
   );
 }
