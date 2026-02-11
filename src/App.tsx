2b3feea8c48ce07253ae635d5bbf61516c1487a4
badlandschugs <robloxfan2017@icloud.com>
2026-02-10 22:13:20 -0500

    added navigation bar (non-functional)

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,8 @@
-import "./App.css";
-
+import NavBar from "./components/NavBar";
 function App() {
   return (
     <div>
-      <h1>Bointerbrate V3</h1>
-
-      <nav>
-        <div className="navLinks">
-          <a>Home</a>
-          <a>List</a>
-          <a>Rules</a>
-          <a>Submissions</a>
-        </div>
-      </nav>
+      <NavBar />
     </div>
   );
 }
